b4b26aed8d3ab2e26e8fb7968b81002a18a14519
AdiM <adimaskey0@gmail.com>
2024-04-04 22:39:26 -0400

    Added the shoe adding page

--- JS-Files/addShoes.js
+++ JS-Files/addShoes.js
@@ -1,17 +1,33 @@
-document.getElementById('shoeForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
+const reviewMessageDiv = document.getElementById("reviewMessage");
+const form = document.getElementById("formInput");
 
-    
-    var sName = document.getElementById('sName').value.trim();
-    var color = document.getElementById('color').value.trim();
-    var condition = document.querySelector('input[name="condition"]:checked');
-    var description = document.getElementById('description').value.trim();
-    var shoesImage = document.getElementById('shoesImage').value.trim();
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Validate form fields
+    let sName = document.getElementById('sName').value.trim();
+    let color = document.getElementById('color').value.trim();
+    let condition = document.querySelector('input[name="condition"]:checked');
+    let description = document.getElementById('description').value.trim();
+    let shoesImage = document.getElementById('shoesImage').value.trim();
 
     if (!sName || !color || !condition || !description || !shoesImage) {
       alert('Please fill in all fields.');
       return;
     }
 
-    this.reset();
-});
+    var submittedDataParagraph = document.getElementById('submittedData');
+
+    submittedDataParagraph.innerHTML = `
+      <strong>Shoe/Brand Name:</strong> ${sName}<br>
+      <strong>Color:</strong> ${color}<br>
+      <strong>Condition:</strong> ${condition.value}<br>
+      <strong>Description:</strong> ${description}<br>
+      <strong>Shoes Image:</strong> ${shoesImage}<br>
+    `;
+
+    reviewMessageDiv.style.display = 'block';
+
+    // Hide the form
+    form.style.display = 'none';
+  });
